f6777dcd4e6acc38f3af1013fbd3ae8e8b9141f9
yiyah <756307810@qq.com>
2020-08-01 18:04:16 +0800

    modify file2 and del html

--- learn/novel/xbiquge/2_getCatalogue.py
+++ learn/novel/xbiquge/2_getCatalogue.py
@@ -10,27 +10,34 @@ def getCatalogueAndLink(url):
     chapter = []
     chapter_Link = []
 
+    # step1. get html
     html = requests.get(url)
     html.encoding = 'utf-8'
 
+    # step2. parse html
     soup = BeautifulSoup(html.text, 'lxml')
+    # get title
     title = soup.find_all('h1')
+    # locate the chapter
     texts = soup.find_all('div', id="list")
-    texts = str(texts[0])
-    soup = BeautifulSoup(texts, 'lxml')
+    # parse the string which contained the chapter and it's link
+    soup = BeautifulSoup(str(texts[0]), 'lxml')
+    # find all a label as list
     a_labels = soup.find_all('a')
+
+    # step3. save them 
     hostIP = "https://www.xsbiquge.com"
     for a_item in a_labels:
         chapter.append(a_item.string)
         chapter_Link.append(hostIP+a_item.get("href"))
 
-    return title, chapter, chapter_Link
+    return title[0].text, chapter, chapter_Link
 
 
 def main():
     title, chapter, chapter_Link = getCatalogueAndLink(mainPage)
 
-    with open("html.html", 'w') as f:
+    with open(title+".html", 'w') as f:
         for i in range(len(chapter)):
             f.write("%s --> %s\n" % (chapter[i], chapter_Link[i]))
 
